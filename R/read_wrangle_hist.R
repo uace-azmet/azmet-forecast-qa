ea1af23d8b924aed7939601de5580187884f016e
Eric R. Scott <scottericr@gmail.com>
2022-11-01 10:29:41 -0400

    filter out inactive stations

--- R/read_wrangle_hist.R
+++ R/read_wrangle_hist.R
@@ -1,5 +1,6 @@
 read_wrangle_hist <- function(hist_file) {
   read_csv(hist_file) |>
     # missing values in historic data coded as 999
-    mutate(across(where(is.numeric), \(x) if_else(x == 999, NA_real_, x)))
+    mutate(across(where(is.numeric), \(x) if_else(x == 999, NA_real_, x))) |> 
+    filter(meta_station_id %in% azmetr::station_info$meta_station_id)
 }
