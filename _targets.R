bd26ab1dfed2dcc34c174556f4c5434e58e90ac4
Eric R. Scott <scottericr@gmail.com>
2022-12-22 14:39:43 -0500

    rename

--- _targets.R
+++ _targets.R
@@ -109,7 +109,7 @@ tar_plan(
 
   # Reports -----------------------------------------------------------------
   #TODO: move some of the long-running code in the report to targets?
-  tar_quarto(report, "docs/report.qmd"),
+  tar_quarto(report, "docs/QA-report.qmd"),
   tar_quarto(readme, "README.qmd")
   
 )
