5836dc631c0d89cb34a4d519b3404b88ab9f539c
Eric R. Scott <scottericr@gmail.com>
2023-02-03 17:57:38 -0500

    remove hourly stuff completely

--- _targets.R
+++ _targets.R
@@ -61,44 +61,10 @@ tar_plan(
     deployment = "main"
   ), 
   
-  # tar_target(db_daily, 
-  #            update_daily("data/daily", db_daily_init), #also writes to data/daily
-  #            #This target becomes invalid if it hasn't been run for a day
-  #            cue = tarchetypes::tar_cue_age(
-  #              name = db_daily,
-  #              age = as.difftime(1, units = "days") 
-  #            ),
-  #            format = "file", #function just returns a path, so track the actual files
-  #            deployment = "main"), #don't run on parallel worker
-  # 
-  # 
-  # #hourly
-  # hourly_start = "2020-12-30 00",
-  # 
-  # # Writes to data store, but returns a tibble. I think this is necessary for
-  # # this workflow to run, but if you remove the data files manually, it won't
-  # # invalidate this target!
-  # tar_target(db_hourly_init,
-  #   init_hourly(hourly_start),
-  #   deployment = "main"),
-  # 
-  # 
-  # tar_target(
-  #   db_hourly,
-  #   update_hourly("data/hourly", db_hourly_init),
-  #   #This target becomes invalid if it hasn't been run for a day
-  #   cue = tarchetypes::tar_cue_age(
-  #     name = db_hourly,
-  #     age = as.difftime(1, units = "days")
-  #   ),
-  #   format = "file",
-  #   deployment = "main" #don't run on parallel workers
-  # ),
 
   tar_file(metadata_file, "data/azmet-data-metadata.xlsx"),
   needs_qa_daily = needs_qa(metadata_file, "daily"),
-  # needs_qa_hourly = needs_qa(metadata_file, "hourly"),
-  # 
+
   # Modeling ----------------------------------------------------------------
   # Limit forecast-based validation to just variables that are appropriate for
   # this kind of modeling.
