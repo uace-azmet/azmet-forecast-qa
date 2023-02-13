ac80098bf8bd2bb092948179cb2ea77b09d4ce10
Eric R. Scott <scottericr@gmail.com>
2023-02-13 09:44:33 -0500

    move anonymous function to real function so dependencies are detected correctly.

---
+++ R/pin_forecast.R
@@ -0,0 +1,19 @@
+#' Update pin of forecast dataset on Connect
+#' 
+#' Pulls down the previous version of the pin, adds any new rows to the top, and
+#' overwrites the pin. Pin is versioned, so previous versions can be inspected
+#' on Posit Connect.
+#'
+#' @param fc_daily the wrangled forecast dataset target
+#' @param name location of pin on board.  Default is the location published by
+#'   Eric Scott under the name "fc_daily"
+#'
+pin_forecast <- function(fc_daily, name = "ericrscott/fc_daily") {
+  board <- board_connect()
+  #read pin
+  old <- board |> pin_read(name)
+  #add new forecast data
+  fc_daily <- bind_rows(fc_daily, old) |> distinct()
+  #update pin
+  board |> pin_write(fc_daily, name)
+}
--- _targets.R
+++ _targets.R
@@ -124,17 +124,8 @@ tar_plan(
   
   tar_target(
     pin_fc,
-    \(fc_daily) {
-      board <- board_connect()
-      #read pin
-      old <- board |> pin_read("ericrscott/fc_daily")
-      #add new forecast data
-      fc_daily <- bind_rows(fc_daily, old) |> distinct()
-      #update pin
-      board |> pin_write(fc_daily, name = "ericrscott/fc_daily")
-    },
+    pin_forecast(fc_daily),
     deployment = "main"
-
   ),
 
   # Reports -----------------------------------------------------------------
